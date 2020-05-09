8f2fef718db05e7e5659c8e121e1351ebfaeaf91
Diego Ferrari <dferrari@litebox.ai>
2020-05-09 16:02:17 -0300

    added redirect to home to logo container on header

--- src/views/components/header/index.jsx
+++ src/views/components/header/index.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import { useHistory } from "react-router-dom";
 import style from './style.module.scss'
+import ROUTES from '../../../constants/routes'
 import TEXTS from '../../../constants/texts'
 
 const Header = () => {
+  const history = useHistory()
+
+  const handleClick = () => {
+    history.push(ROUTES.homepage)
+  }
 
     return (
       <div className={style.headerContainer}>
-        <div className={style.flag} />
-        <div className={style.headerTitle}>{TEXTS.headerTitle}</div>
+        <div onClick={() => handleClick()} className={style.logoContainer}>
+          <div/>
+          <div>{TEXTS.headerTitle}</div>
+        </div>
       </div>
     );
 }
