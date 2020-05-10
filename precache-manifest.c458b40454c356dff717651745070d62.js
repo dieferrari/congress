f952b9295e11bac4bc331bd9bfcceb49c0610782
Diego Ferrari <dferrari@litebox.ai>
2020-05-10 18:04:18 -0300

    Updates

---
+++ precache-manifest.c458b40454c356dff717651745070d62.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d56b2924715ffa67f4ffac7a9971fc7",
+    "url": "/congress/index.html"
+  },
+  {
+    "revision": "f6bf50637c7919838693",
+    "url": "/congress/static/css/Homepage.55bb3f22.chunk.css"
+  },
+  {
+    "revision": "0bfc8c9344fb19041a5d",
+    "url": "/congress/static/css/Layout.faa6f3d2.chunk.css"
+  },
+  {
+    "revision": "2d07ffec39882ebb085f",
+    "url": "/congress/static/css/Profile.be309aa1.chunk.css"
+  },
+  {
+    "revision": "cc2896bddf34ac08cc0d",
+    "url": "/congress/static/css/main.c5bcf5fb.chunk.css"
+  },
+  {
+    "revision": "0cf8060df9cfffc79b41",
+    "url": "/congress/static/js/0.2dd2f32d.chunk.js"
+  },
+  {
+    "revision": "c7a62a9843f3102ebd7a",
+    "url": "/congress/static/js/6.e9a3e16a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/congress/static/js/6.e9a3e16a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6bf50637c7919838693",
+    "url": "/congress/static/js/Homepage.ab25952e.chunk.js"
+  },
+  {
+    "revision": "0bfc8c9344fb19041a5d",
+    "url": "/congress/static/js/Layout.9264c3c2.chunk.js"
+  },
+  {
+    "revision": "2d07ffec39882ebb085f",
+    "url": "/congress/static/js/Profile.6d584666.chunk.js"
+  },
+  {
+    "revision": "cc2896bddf34ac08cc0d",
+    "url": "/congress/static/js/main.01f5d4f3.chunk.js"
+  },
+  {
+    "revision": "f4fb2da40dbc708d337c",
+    "url": "/congress/static/js/runtime-main.55062ce1.js"
+  },
+  {
+    "revision": "bfcf6d779e2eab129c0dedeb916f9343",
+    "url": "/congress/static/media/back-icon.bfcf6d77.svg"
+  },
+  {
+    "revision": "ad4a5d94b2f01cd49e8232ead71c8e37",
+    "url": "/congress/static/media/chevron-icon.ad4a5d94.svg"
+  },
+  {
+    "revision": "f2d27c4d8963cee850ff125df5d3efc8",
+    "url": "/congress/static/media/female-senator-placeholder.f2d27c4d.svg"
+  },
+  {
+    "revision": "31d2c5f198bf6f7d8fc7f87fc2bae249",
+    "url": "/congress/static/media/male-senator-placeholder.31d2c5f1.svg"
+  },
+  {
+    "revision": "91b18b20b357e6ac84200df55362751f",
+    "url": "/congress/static/media/search-icon.91b18b20.svg"
+  },
+  {
+    "revision": "ad5dff63fcfd4539295434c0013c3afa",
+    "url": "/congress/static/media/us-flag.ad5dff63.svg"
+  }
+]);
